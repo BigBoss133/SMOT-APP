adf86939c6881b169ee963e0df8f014520c1455e
BigBoss133 <99394318+BigBoss133@users.noreply.github.com>
2026-05-11 19:14:43 +0200

    feat(frontend): wrap app with ToastProvider

--- smot-desktop/src/main.tsx
+++ smot-desktop/src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { LanguageProvider } from "./context/LanguageContext";
+import { ToastProvider } from "./context/ToastContext";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <LanguageProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ToastProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ToastProvider>
     </LanguageProvider>
   </StrictMode>,
 );
